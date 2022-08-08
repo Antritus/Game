daf1b0da30e957f33388a4e45bb3dacfc4860d5d
Antritus <antt.graft@gmail.com>
2022-08-08 09:27:21 +0300

    Update OBJ_Diamond_Key.java
    change main to entity.object.Object

--- Project/src/main/java/object/OBJ_Diamond_Key.java
+++ Project/src/main/java/object/OBJ_Diamond_Key.java
@@ -5,7 +5,7 @@ import entity.Entity;
 import javax.imageio.ImageIO;
 import java.io.IOException;
 
-public class OBJ_Diamond_Key extends Entity {
+public class OBJ_Diamond_Key extends entity.object.Object {
 	public OBJ_Diamond_Key() {
 		setName("DiamondKey");
 		try {
